6e0def13d0a61ed94b50e7b657ebb3123cabafe6
Tanweer Raza <tanatif@gmail.com>
2023-03-22 02:32:45 +0530

    V 1.1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt =models.CharField(max_length=500)
+    image_name = models.CharField(max_length=80)
+    date = models.DateField(auto_now=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    slug = models.SlugField(unique=True, default="",blank=True,
+                            null=False, db_index=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null = True, related_name="post") # one to many reltionship
+    tags = models.ManyToManyField(Tag)
+
+    # def __str__(self) -> str:
+    #     return self.title + " Blog"
+
+     
+
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,144 @@
 from django.shortcuts import render
+from datetime import date
 
+all_posts = [
+        {
+        "slug":"hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author":'T.Raza',
+        "date" : date(2023, 3, 16),
+        "title" : " 2.Mountain Hiking",
+        "excerpt" : """
+                    There's nothing like the views you get when hiking in the mountains! 
+                    And I wasn't even prepared for what happened whilst I was enjoying the view!
+                    """ ,
+        "content" : """
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+                    """
+
+        },
+
+        {
+        "slug":"hike-in-the-woods",
+        "image": "woods.jpg",
+        "author":'T.Raza',
+        "date" : date(2023, 6, 18),
+        "title" : "4.Mountain Hiking",
+        "excerpt" : """
+                    There's nothing like the views you get when hiking in the mountains! 
+                    And I wasn't even prepared for what happened whilst I was enjoying the view!
+                    """ ,
+        "content" : """
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+                    """
+
+         },
+        {
+        "slug":"hike-in-max-world",
+        "image": "max.png",
+        "author":'T.Raza',
+        "date" : date(2023, 5, 26),
+        "title" : "3.Mountain Hiking",
+        "excerpt" : """
+                    There's nothing like the views you get when hiking in the mountains! 
+                    And I wasn't even prepared for what happened whilst I was enjoying the view!
+                    """ ,
+        "content" : """
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+                    """
+
+         },
+        {
+        "slug":"hike-in-the-tan-world",
+        "image": "tan.jpg",
+        "author":'T.Raza',
+        "date" : date(2023, 1, 23),
+        "title" : "1.Mountain Hiking",
+        "excerpt" : """
+                    There's nothing like the views you get when hiking in the mountains! 
+                    And I wasn't even prepared for what happened whilst I was enjoying the view!
+                    """ ,
+        "content" : """
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+
+                    INNER JOIN will fetch only those records which are present in both the joined tables.
+                    The matching of the records is only based on the columns used for joining these two tables.
+                    INNER JOIN can also be represented as JOIN in your SELECT query.
+                    """
+
+        }
+]
+
+
+def get_date(post):
+    return post['date']
 # Create your views here.
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date , reverse = True)
+    latest_posts = sorted_posts[:3]
+    return render(request, "blog/index.html" ,{
+        "posts" : latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts" : all_posts
+    })
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html" ,{
+        "post" : identified_post
+    })
