b27f05639b701a4c3dfae7548865b5df8f7f20aa
Tanweer Raza <tanatif@gmail.com>
2023-03-28 06:37:33 +0530

    Comment model done

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" , views.starting_page , name="starting-page"),
-    path("posts", views.posts, name= "posts-page"),
-    path("posts/<slug:slug>", views.post_details, name="post_detail-page"),   # /posts/my-first-post
+    path("" , views.StartingPageView.as_view() , name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name= "posts-page"),
+    path("posts/<slug:slug>", views.PostDetail.as_view(), name="post_detail-page"),   # /posts/my-first-post
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,78 @@
 from django.shortcuts import render ,get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.views import View
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
-
+from .forms import CommentForm
 # Create your views here.
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3] # - for descending order
-    return render(request, "blog/index.html" ,{
-        "posts" : latest_posts
-    })
 
-def posts(request):
-    return render(request, "blog/all-posts.html", {
-        "all_posts" : Post.objects.all().order_by("-date")
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering =["-date"]
+    context_object_name = "posts"
 
-def post_details(request, slug):
-    identified_post = get_object_or_404(Post, slug = slug)
-    return render(request, "blog/post-detail.html" ,{
-        "post" : identified_post,
-        "post_tags" : identified_post.tags.all()
-    })
+    def get_queryset(self) :
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3] # - for descending order
+#     return render(request, "blog/index.html" ,{
+#         "posts" : latest_posts
+#     })
+
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# def posts(request):
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts" : Post.objects.all().order_by("-date")
+#     })
+
+class PostDetail(View):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug = slug)
+        context = {
+         "post" : post,
+         "post_tags" : post.tags.all(),
+         "comment_form" : CommentForm(),
+         "comments" : post.comments.all().order_by("-id")
+        }
+        
+        return render(request, "blog/post-detail.html", context) 
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug = slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post                 # saving the comment to respective post 
+            comment.save()
+            return HttpResponseRedirect(reverse("post_detail-page", args=[slug]))
+        
+    
+        context = {
+         "post" : post,
+         "post_tags" : post.tags.all(),
+         "comment_form" : comment_form,
+         "comments" : post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context) 
+
+# def post_details(request, slug):
+#     identified_post = get_object_or_404(Post, slug = slug)
+#     return render(request, "blog/post-detail.html" ,{
+#         "post" : identified_post,
+#         "post_tags" : identified_post.tags.all()
+#     })
